4a5c8e43a5d82d2d5279cf1d3be397000ebbc4e9
crawlin <crawlin@uw.edu>
2024-10-04 09:53:25 -0700

    pushed header bar things to the right

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ export function NavBar() {
             <button 
                 key={name}
                 onClick={() => navigate(name === "Overview" ? '/' : `/${encodeURIComponent(name.toLowerCase().replace(/\s/g, ''))}`)}
-                className={`justify-center items-center flex w-full`}
+                className={`items-center flex w-full`}
             >
                 <p className={`${currentSection.trim() === name.toLowerCase().replace(/\s/g, '') ? "font-bold" : "font-thin"} text-sm text-left px-2 text-blue-50`}>{name}</p>
             </button>
@@ -24,13 +24,11 @@ export function NavBar() {
     }
 
     return (
-        <nav className="flex flex-row w-full bg-blue-950 gap-5 py-4 px-6 border-b border-slate-300 text-nowrap">
-            <p className={`text-left text-blue-50 font-thin mr-8 text-xl`}>UW CSE/ECE 475</p>
-            {sections.map((section) => {
-                return (
-                    navBarButton(section)
-                )
-            })}
+        <nav className="flex flex-row w-full bg-blue-950 py-4 px-6 border-b border-slate-300 text-nowrap">
+            <button onClick={() => navigate("/")} className={`text-left text-blue-50 font-thin mr-8 text-xl`}>UW CSE/ECE 475</button>
+            <div className="flex flex-row ml-auto gap-5">
+                {sections.map((section) => navBarButton(section))}
+            </div>
         </nav>
     )
 }
