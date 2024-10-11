ef5de49781eeca2e5385e52e7c40e5beb2a230a7
crawlin <crawlin@uw.edu>
2024-10-10 22:30:40 -0700

    changed spacing for page header

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export function PageLayout({title, content}) {
     return (
         <main className="flex flex-col w-full items-center md:px-8 lg:px-20 xl:px-28">
-            <h1 className="font-h1 pt-16 text-h1 mb-3 text-center">{title}</h1>
+            <h1 className="font-h1 pt-16 text-h1 my-4 text-center">{title}</h1>
             {content}
         </main>
     )
