b1a8758b32830d1a58fe046201d33cc308d57862
crawlin <crawlin@uw.edu>
2024-12-05 14:10:33 -0800

    added another accuacy matrix to test page

--- src/pages/Testing.tsx
+++ src/pages/Testing.tsx
@@ -4,22 +4,28 @@ export function Testing() {
     
 function pictureBox(image) {
     return (
-        <div className="h-[40rem] w-full">
+        <div className="h-[20rem] ">
             {image}
         </div>
     )
 }
     return (
         <div className={"flex flex-col gap-2 mb-[20rem]"}>
-
-            {pictureBox(<img className="h-full w-auto object-contain" src={`${process.env.PUBLIC_URL}/media/Figure_1.png`} alt="" />)}
-            <p className="mb-2">This is the accuaracy matrix of the model we trained on gesture data.</p> 
+            <div className="flex flex-row gap-2">
+                {pictureBox(<img className="h-full w-auto object-contain" src={`${process.env.PUBLIC_URL}/media/ml1.png`} alt="" />)}
+                {pictureBox(<img className="h-full w-auto object-contain" src={`${process.env.PUBLIC_URL}/media/ml2.png`} alt="" />)}
+            </div>
+            <p className="mb-10">These are iterations of the ML models accuracy. We were able to improve it by changing the type of data being collected and by making the gestures simplier.</p> 
 
             <div className="flex flex-row gap-2">
                 {pictureBox(<img className="h-full w-auto object-contain"  src={`${process.env.PUBLIC_URL}/media/fittsLawTouchpad.png`} alt="" />)}
                 {pictureBox(<img className="h-full w-auto object-contain" src={`${process.env.PUBLIC_URL}/media/fittsLawGlove.png`} alt="" />)}
             </div>
-            These tables compare the accuracy our glove to a touchpad using a Fitts law test.
+            <p> These tables compare the accuracy our glove to a touchpad using a Fitts law test.</p>
+
+        
+
+
         </div>
     )
 
